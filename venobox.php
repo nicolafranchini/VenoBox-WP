9d4f2664591f03a3c1cf43c109cf78ea35996020
Nicola Franchini <info@veno.es>
2025-02-05 13:50:02 +0000

    Version update

--- venobox.php
+++ venobox.php
@@ -5,7 +5,7 @@
  * Description: Modal windows for images, videos, inline contents, iFrames, Ajax requests. Touch swipe galleries.
  * Author: Nicola Franchini
  * Author URI: https://veno.es
- * Version: 1.0.6
+ * Version: 1.0.7
  * License: GPL-2.0+
  * License URI: http://www.gnu.org/licenses/gpl-2.0.txt
  * Text Domain: venobox
@@ -17,7 +17,7 @@
 if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
-define( 'VBOX_VENOBOX_PLUGIN_VERSION', '1.0.6' );
+define( 'VBOX_VENOBOX_PLUGIN_VERSION', '1.0.7' );
 if ( ! class_exists( 'VenoBox_Plugin', false ) ) {
 	require_once __DIR__ . '/include/class-venobox-plugin.php';
 }
